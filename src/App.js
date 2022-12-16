1d598b85cbfa03d3acd582d9c7c7dbfa3c48765e
amayamaya <aamaya547@gmail.com>
2022-12-15 21:02:28 -0800

    Added App.js and fetch utils to cret getPantones

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { getPantones } from './services/fetch-utils';
+import PantoneList from './PantoneList';
+
 import './App.css';
 
 function App() {
+  const [pantones, setPantones] = useState([]);
+
+  useEffect(() => {
+    async function fetchPantonesData() {
+      const pantoneData = await getPantones();
+      setPantones(pantoneData);
+    }
+
+    fetchPantonesData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Pantone of the Year</h1>
+        <PantoneList pantones={pantones} />
       </header>
-      <div className="pantone-grid"></div>
     </div>
   );
 }
